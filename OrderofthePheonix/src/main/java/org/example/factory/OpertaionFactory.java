c57efb8d2a2b1dac173618d466125c04c32d135d
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-25 11:53:13 +0530

    Student factory added

--- OrderofthePheonix/src/main/java/org/example/factory/OpertaionFactory.java
+++ OrderofthePheonix/src/main/java/org/example/factory/OpertaionFactory.java
@@ -11,17 +11,21 @@ public class OpertaionFactory {
 
     public void selectItemFromMainMenu(int number) {
 
-        HostelManagementMenu hostelManagementMenu = new HostelManagementMenu();
-
         if(number == 1) {
+            HostelManagementMenu hostelManagementMenu = new HostelManagementMenu();
+            int result = hostelManagementMenu.hostelMenu();
             HostelMenuFactory hostelMenuFactory = new HostelMenuFactory();
-            hostelMenuFactory.selectMenuItem(number);
+            hostelMenuFactory.selectMenuItem(result);
         } else if (number == 2) {
+            StudentManagementMenu studentManagementMenu = new StudentManagementMenu();
+            int result = studentManagementMenu.studentMenu();
             StudentMenuFactory studentMenuFactory = new StudentMenuFactory();
-            studentMenuFactory.selectMenuItem(number);
+            studentMenuFactory.selectMenuItem(result);
         } else if (number == 3) {
+            StudentAllocationManagementMenu studentAllocationManagementMenu = new StudentAllocationManagementMenu();
+            int result = studentAllocationManagementMenu.studentAllocationMenu();
             StudentAllocationMenuFactory studentAllocationMenuFactory = new StudentAllocationMenuFactory();
-            studentAllocationMenuFactory.selectMenuItem(number);
+            studentAllocationMenuFactory.selectMenuItem(result);
         } else if(number == 0) {
             return;
         } else {
