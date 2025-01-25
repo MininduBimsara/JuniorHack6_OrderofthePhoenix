dc4d4d2af0346b8527f1fd51e455da983b865853
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-25 10:33:42 +0530

    Student allocation management menu added

---
+++ OrderofthePheonix/src/main/java/org/example/menus/StudentAllocationManagementMenu.java
@@ -0,0 +1,18 @@
+package org.example.menus;
+import java.util.Scanner;
+
+public class StudentAllocationManagementMenu {
+    public int studentMenu() {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Student Allocation Management...\n\n");
+        System.out.println("[1] Allocate a Room to a Student\n\n");
+        System.out.println("[1] Delete an Allocation\n\n");
+        System.out.println("[1] View an Allocation\n\n");
+        System.out.println("[0] Main Menu\n\n");
+
+        int numberStudentAllocation = input.nextInt();
+
+        return numberStudentAllocation;
+    }
+}
