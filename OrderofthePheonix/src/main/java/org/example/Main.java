35cf2e0bc1ec98eb1a5a7afdc687072381b33278
Ramitha Iddamalgoda <ramithapathmilarp@gmail.com>
2025-01-25 09:51:53 +0530

    initial commit

--- OrderofthePheonix/src/main/java/org/example/Main.java
+++ OrderofthePheonix/src/main/java/org/example/Main.java
@@ -1,17 +1,31 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.Services.MainService;
+import org.example.Utils.DatabaseIntegration;
+
+import java.sql.Connection;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        // Connect to the database
+
+        Connection conn = DatabaseIntegration.getConnection();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\n---------Welcome to the Hostel Management System---------\n\n");
+        System.out.println("\n\n---------ABC University---------\n\n");
+        System.out.println("Please enter the number of the action you want to proceed\n\n");
+        System.out.println("[1] Manage Hostels\n\n");
+        System.out.println("[2] Manage Students\n\n");
+        System.out.println("[3] Manage Student Allocations\n\n");
+        System.out.println("[0] Exit\n\n");
+        String name = input.nextLine();
+
+
+        // Add an employee to the database
+        MainService mainService = new MainService();
     }
 }
---
+++ OrderofthePheonix/src/main/java/org/example/menus/HostelManagementMenu.java
@@ -0,0 +1,12 @@
+package org.example.menus;
+
+public class HostelManagementMenu {
+    public void showMenu() {
+        System.out.println("Hostel Management...\n\n");
+        System.out.println("[1] Add a Hostel\n\n");
+        System.out.println("[2] Update Hostel Information\n\n");
+        System.out.println("[3] View Hostel Details\n\n");
+        System.out.println("[4] Rooms Management\n\n");
+        System.out.println("[0] Main Menu\n\n");
+    }
+}
