2442e893a207fb3fcc8066bc1e75745f86af7611
ima-69 <imansha.idr@gmail.com>
2025-01-25 11:41:10 +0530

    studentallocaton crud

--- OrderofthePheonix/src/main/java/org/example/Services/StudentAllocationService.java
+++ OrderofthePheonix/src/main/java/org/example/Services/StudentAllocationService.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 public class StudentAllocationService {
 
     public void allocateRoom(String allocation_id, String student_id, String room_id,String date, Connection conn) {
-        // Add a Student to the database
+        // Add a allocation to the database
         String sql = "INSERT INTO allocations (allocation_id, student_id, room_id, date) VALUES (?, ?, ?, ?)";
 
         try {
@@ -27,7 +27,7 @@ public class StudentAllocationService {
     }
 
     public void deleteAllocation(String allocation_id, Connection conn) {
-        // Delete a Student from the database
+        // Delete a allocation from the database
         String sql = "DELETE FROM allocations WHERE allocation_id = ?";
 
         try {
@@ -41,49 +41,32 @@ public class StudentAllocationService {
 
     }
 
-    public void updateStudent(String student_id, String name, int age,String department, Connection conn) {
-        // Update a Student in the database
-        String sql = "UPDATE students SET student_id = ?, name = ?, age = ?, department = ? WHERE student_id = ?";
+    public void getHostel(String allocation_id, Connection conn) {
+        // Get a allocation from the database
+        String sql = "SELECT * FROM allocations WHERE allocation_id = ?";
 
         try {
             PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            preparedStatement.setString(1, student_id);
-            preparedStatement.setString(2, name);
-            preparedStatement.setInt(3, age);
-            preparedStatement.setString(4, department);
-            preparedStatement.setString(5, student_id);
-            preparedStatement.executeUpdate();
-            System.out.println("Student updated successfully");
-        } catch (SQLException e) {
-            System.out.println("An error occurred while updating an student in the database " + e.getMessage());
-        }
-    }
-    public void getHostel(String student_id, Connection conn) {
-        // Get a Student from the database
-        String sql = "SELECT * FROM students WHERE student_id = ?";
-
-        try {
-            PreparedStatement preparedStatement = conn.prepareStatement(sql);
-            preparedStatement.setString(1, student_id);
+            preparedStatement.setString(1, allocation_id);
             preparedStatement.executeQuery();
-            System.out.println("Studnet retrieved successfully");
+            System.out.println("Allocation retrieved successfully");
 
             ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
-                int studentId = resultSet.getInt("student_id");
-                String studentName = resultSet.getString("name");
-                int studentAge = resultSet.getInt("age");
-                int studentDepartment = resultSet.getInt("department");
+                String allocationId = resultSet.getString("allocation_id");
+                String studentId = resultSet.getString("student_id");
+                String roomId = resultSet.getString("room_id");
+                String date = resultSet.getString("date");
+                System.out.println("Allocation ID: " + allocationId);
                 System.out.println("Student ID: " + studentId);
-                System.out.println("Student Name: " + studentName);
-                System.out.println("Student Age: " + studentAge);
-                System.out.println("Student Department: " + studentDepartment );
+                System.out.println("Room ID: " + roomId);
+                System.out.println("Date: " + date);
             } else {
-                System.out.println("Student not found");
+                System.out.println("Allocation not found");
             }
 
         } catch (SQLException e) {
-            System.out.println("An error occurred while retrieving an Student from the database " + e.getMessage());
+            System.out.println("An error occurred while retrieving an allocation from the database " + e.getMessage());
         }
 
     }
